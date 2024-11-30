10674afcf276c144cda7a871fbb409f956b04408
arslanashraf006 <arslanashraf7864@gmail.com>
2024-11-30 13:17:28 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+// Importing necessary packages and files
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_clean_coding/repository/auth_api/auth_api_repository.dart';
+import 'package:flutter_bloc_clean_coding/repository/auth_api/auth_http_api_repository.dart';
+import 'package:flutter_bloc_clean_coding/repository/movies_api/movies_api_repository.dart';
+import 'package:flutter_bloc_clean_coding/repository/movies_api/movies_http_api_repository.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:get_it/get_it.dart'; // Package for dependency injection
+
+import 'configs/routes/routes.dart'; // Custom routes
+import 'configs/routes/routes_name.dart'; // Route names
+import 'configs/themes/dark_theme.dart'; // Dark theme configuration
+import 'configs/themes/light_theme.dart'; // Light theme configuration
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+// GetIt is a package used for service locator or to manage dependency injection
+GetIt getIt = GetIt.instance;
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensuring that Flutter bindings are initialized
+  servicesLocator(); // Initializing service locator for dependency injection
+  runApp(const MyApp()); // Running the application
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}); // Constructor for MyApp widget
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Material app configuration
+      title: 'Flutter Demo',
+      themeMode: ThemeMode.dark, // Setting theme mode to dark
+      theme: lightTheme, // Setting light theme
+      darkTheme: darkTheme, // Setting dark theme
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'), // English locale
+        Locale('es'), // Spanish locale
+      ],
+      initialRoute: RoutesName.splash, // Initial route
+      onGenerateRoute: Routes.generateRoute, // Generating routes
+    );
+  }
+}
+
+// Function for initializing service locator
+void servicesLocator() {
+  getIt.registerLazySingleton<AuthApiRepository>(() => AuthHttpApiRepository()); // Registering AuthHttpApiRepository as a lazy singleton for AuthApiRepository
+  getIt.registerLazySingleton<MoviesApiRepository>(() => MoviesHttpApiRepository()); // Registering MoviesHttpApiRepository as a lazy singleton for MoviesApiRepository
+}
---
+++ lib/view/login/widget/submit_button_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_clean_coding/utils/extensions/flush_bar_extension.dart';
+
+import '../../../bloc/login_bloc/login_bloc.dart';
+import '../../../configs/routes/routes_name.dart';
+import '../../../utils/enums.dart';
+
+/// A widget representing the submit button for the login form.
+class SubmitButton extends StatelessWidget {
+  final formKey;
+  const SubmitButton({Key? key, required this.formKey}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<LoginBloc, LoginStates>(
+      listenWhen: (current, previous) => current.postApiStatus != previous.postApiStatus,
+      listener: (context, state) {
+        if (state.postApiStatus == PostApiStatus.error) {
+          context.flushBarErrorMessage(message: state.message.toString());
+        }
+
+        if (state.postApiStatus == PostApiStatus.success) {
+          Navigator.pushNamedAndRemoveUntil(context, RoutesName.home, (route) => false);
+        }
+      },
+      child: BlocBuilder<LoginBloc, LoginStates>(
+          buildWhen: (current, previous) => current.postApiStatus != previous.postApiStatus,
+          builder: (context, state) {
+            return ElevatedButton(
+                onPressed: () {
+                  if (formKey.currentState.validate()) {
+                    context.read<LoginBloc>().add(LoginApi());
+                  }
+                },
+                child: state.postApiStatus == PostApiStatus.loading ? const CircularProgressIndicator() : const Text('Login'));
+          }),
+    );
+  }
+}
